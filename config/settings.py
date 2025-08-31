3e873c433218556ec4468b8506421362eefc57bd
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 19:25:30 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,21 @@
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "blog/static",
+]
+
+TEMPLATES = [
+    {
+        'DIRS': [BASE_DIR / 'blog/templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
