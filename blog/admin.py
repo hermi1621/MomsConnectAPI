e2e5e82e03c9c3ed46f394a9d6323e2d4a433da2
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 21:54:15 +0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+
+from django.contrib import admin
+from .models import Post, Category, Comment
+
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Comment)
