f6c7024ade20bff3d0b3877a4d6ffcfbd369cc4c
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 19:38:52 +0300

    Create forms.py

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Post
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['content', 'image']
