5fb2b8b095f0e428325273f6b71e5fceb0453584
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 19:55:30 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,26 +5,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
-
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),   # Include your blog app URLs
-]
-
-
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
