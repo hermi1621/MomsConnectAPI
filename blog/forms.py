127b5d225778835163b3c2d0b6f98af5aadf3958
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 21:21:02 +0300

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from .models import Post
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
