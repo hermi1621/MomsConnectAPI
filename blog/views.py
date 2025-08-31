abe14f4c13fea1bfd8c254920fb1fffe60314725
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 19:30:16 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,12 @@ def home(request):
 def posts(request):
     return render(request, 'blog/base.html')
 
+
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'blog/base.html')  # Your HTML page
+
+def posts(request):
+    return render(request, 'blog/base.html')  # Can be same page for now
