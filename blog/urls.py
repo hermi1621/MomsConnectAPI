eb1717e55c2772ce133455642e868cb447a8b108
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 22:01:36 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,19 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Home page – show posts and form to create posts
     path('', views.home, name='home'),
+
+    # User registration
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+
+    # User login
+    path('login/', auth_views.LoginView.as_view(
+        template_name='blog/login.html'
+    ), name='login'),
+
+    # User logout
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='home'
+    ), name='logout'),
 ]
