f4e4117381b47ed9e3ff9231c36c1f75d1cda8c4
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 20:44:57 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,14 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.user.username}: {self.content[:20]}"
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
