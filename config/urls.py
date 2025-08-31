95a2aa07035bc3f112046c1f6fb19fc824f3e041
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 19:29:47 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,3 +6,12 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),   # Include your blog app URLs
+]
+
