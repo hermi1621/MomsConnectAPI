9cfbc9cbd952d0ce8434ab95fd8983d8dd6e0e2c
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 21:49:17 +0300

    Update scripts.js

--- blog/static/blog/js/scripts.js
+++ blog/static/blog/js/scripts.js
@@ -1,7 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('MomsConnect page loaded');
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('MomsConnect loaded');
-});
