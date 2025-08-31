2aea9cc4895d6275d8dda8c35761c66bfe66842e
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 19:55:08 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,23 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('posts/', views.posts, name='posts'),
-]
-
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),      # Home page
-    path('posts/', views.posts, name='posts')  # Posts page
-]
-
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
 ]
