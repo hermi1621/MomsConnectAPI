360b413015656a2d5258acdde7b076bbe08c9618
hermi1621 <hermelabezabih4@gmail.com>
2025-09-07 15:21:18 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -18,22 +18,11 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(
         next_page='home'
     ), name='logout'),
-]
 
-
-from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    # Edit and delete posts
     path('post/edit/<int:pk>/', views.edit_post, name='edit_post'),
     path('post/delete/<int:pk>/', views.delete_post, name='delete_post'),
+
+    # User profile
+    path('profile/<str:username>/', views.profile, name='profile'),
 ]
-
-
-path('profile/<str:username>/', views.profile, name='profile'),
-
