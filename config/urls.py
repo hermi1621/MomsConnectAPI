e148e21ba15831675afca31c354cd3ebc495efc1
hermi1621 <hermelabezabih4@gmail.com>
2025-09-07 17:00:04 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('blog.urls')),  # include blog app URLs
+    path('', include('blog.urls')),  # <-- no prefix
 ]
 
-# Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
